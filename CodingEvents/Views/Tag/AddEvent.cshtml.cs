8cbce600f7171e6a1453b84a0cfa0356b33d935d
e-bold <enkhmunkh@att.net>
2024-06-22 16:49:28 -0500

    switched branch orm2

---
+++ CodingEvents/Views/Tag/AddEvent.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class AddEventModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CodingEvents/Views/Tag/Detail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class DetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
